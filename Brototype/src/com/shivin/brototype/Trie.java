f0172b2399a29e889b58487ae0a985b411c211c1
Shivin Antony <98376508+ShivinAntony@users.noreply.github.com>
2024-02-06 15:07:36 +0530

    Add files via upload

---
+++ Brototype/src/com/shivin/brototype/Trie.java
@@ -0,0 +1,61 @@
+package com.shivin.brototype;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Trie {
+	
+	static class TrieNode {
+		Map<Character, TrieNode> children = new HashMap<>();
+	}
+	
+	TrieNode root = new TrieNode();
+	char endSymbol ='*';
+	
+	public Trie(String str) {
+		populateSuffixTrie(str);
+	}
+	
+	public void populateSuffixTrie(String str) {
+		
+		for(int i=0;i<str.length();i++) {
+			insertSubstringStartingAt(i, str);
+		}
+		
+	}
+	
+	public void insertSubstringStartingAt(int index,String str) {
+		TrieNode node = root;
+		for(int i=index; i<str.length();i++) {
+			char letter = str.charAt(i);
+			if(!node.children.containsKey(letter)) {
+				TrieNode newNode = new TrieNode();
+				node.children.put(letter, newNode);
+			} 
+				node = node.children.get(letter);
+			
+		}
+		node.children.put(endSymbol, null);
+		
+	}
+	
+	public boolean contains(String str) {
+		TrieNode node =root;
+		for(int i=0;i<str.length();i++) {
+			char letter = str.charAt(i);
+			if(!node.children.containsKey(letter)) {
+				return false;
+			}
+			node = node.children.get(letter);
+		}
+		return node.children.containsKey(endSymbol);
+	}
+	
+	public static void main(String[] args) {
+		
+		Trie  trie = new Trie("mannan");
+		System.out.println(trie.contains("annan"));
+		
+	}
+
+}
